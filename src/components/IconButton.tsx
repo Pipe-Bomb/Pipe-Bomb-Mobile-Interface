da3affb175c3598b5c49ddc77765492176b35f8e
eyezah (via Perfect Girlfriend) <git@eyezah.com>
2023-04-01 18:29:50 +1300

    Style popup player

--- src/components/IconButton.tsx
+++ src/components/IconButton.tsx
@@ -1,17 +1,17 @@
 import { Button } from "@nextui-org/react";
 import { IconContext } from "react-icons";
-import iconStyle from "../styles/Icon.module.scss";
 import styles from "../styles/IconButton.module.scss";
 
 export interface IconButtonProps {
     children?: JSX.Element
     small?: boolean,
+    background?: boolean,
     onPress?: () => void
 }
 
-export default function IconButton({ children, small, onPress }: IconButtonProps) {
+export default function IconButton({ children, small, onPress, background }: IconButtonProps) {
     return (
-        <Button light className={styles.button + (small ? (" " + styles.small) : "")} onPress={onPress}>
+        <Button light className={styles.button + (small ? ` ${styles.small}` : "") + (background ? ` ${styles.background}` : "")} onPress={onPress}>
             <IconContext.Provider value={{size: small ? "30px" : "40px"}}>
                 { children }
             </IconContext.Provider>
--- src/components/PlayerCover.tsx
+++ src/components/PlayerCover.tsx
@@ -1,7 +1,11 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import styles from "../styles/PlayerCover.module.scss";
 import IconButton from "./IconButton";
 import { RxCaretDown } from "react-icons/rx";
+import AudioPlayer, { AudioPlayerStatus } from "../logic/AudioPlayer";
+import { TrackMeta } from "pipebomb.js/dist/music/Track";
+import { Text } from "@nextui-org/react";
+import { convertArrayToString } from "../logic/Utils";
 
 let toggleFunction: (value: boolean) => void;
 
@@ -10,15 +14,76 @@ export function setPlayerOpen(open: boolean) {
 }
 
 export default function PlayerCover() {
+    const audioPlayer = AudioPlayer.getInstance();
     const [isOpen, setIsOpen] = useState(false);
+    const [status, setStatus] = useState<AudioPlayerStatus>(audioPlayer.getStatus());
+    const [trackMeta, setTrackMeta] = useState<TrackMeta | null>(null);
 
     toggleFunction = setIsOpen;
 
+    useEffect(() => {
+        audioPlayer.registerCallback(setStatus);
+
+        return () => {
+            audioPlayer.unregisterCallback(setStatus);
+        }
+    }, []);
+
+    useEffect(() => {
+        if (!status.track) return;
+
+        status.track.getMetadata().then(setTrackMeta);  
+    }, [status.track]);
+
+    if (isOpen && (!status || !status.track)) {
+        setIsOpen(false);
+    }
+
+    const backgroundImage = (() => {
+        if (!trackMeta) {
+            return; // load
+        }
+        if (!trackMeta.image) {
+            return; // no image
+        }
+        return `url(${trackMeta.image})`;
+    })();
+
+    const percentage = Math.round(status.time / status.duration * 200) / 2;
+
+    function togglePlay() {
+        if (!status) return;
+        if (status.paused) {
+            audioPlayer.play();
+        } else {
+            audioPlayer.pause();
+        }
+    }
+
     return (
-        <div className={styles.container + (isOpen ? "" : ` ${styles.closed}`)}>
-            <IconButton onPress={() => setIsOpen(false)}>
-                <RxCaretDown />
-            </IconButton>
+        <div className={styles.container + (isOpen ? "" : ` ${styles.closed}`) + (status.paused ? "" : ` ${styles.playing}`)} onClick={togglePlay}>
+            <div className={styles.background} style={{
+                backgroundImage,
+                left: `${percentage}%`,
+                transform: `translate(${-percentage}%, -50%)`
+            }}></div>
+            <div className={styles.closeButton}>
+                <IconButton onPress={() => setIsOpen(false)} background small>
+                    <RxCaretDown />
+                </IconButton>
+            </div>
+            <div className={styles.topInfo}>
+                <span>
+                    <Text h2 className={styles.title}>
+                        {trackMeta?.title}
+                    </Text>
+                </span>
+                <span>
+                    <Text h3 className={styles.artist}>
+                        { convertArrayToString(trackMeta?.artists || []) }
+                    </Text>
+                </span>
+            </div>
         </div>
     )
 }
