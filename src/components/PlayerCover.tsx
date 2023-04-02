08c5e319fc1eb8186a5bf0f2a02219a6ce70c86a
eyezah <50851883+eyezahhhh@users.noreply.github.com>
2023-04-03 00:07:15 +1200

    add side scrolling

--- src/components/PlayerCover.tsx
+++ src/components/PlayerCover.tsx
@@ -1,14 +1,8 @@
 import { useEffect, useRef, useState } from "react";
 import styles from "../styles/PlayerCover.module.scss";
-import IconButton from "./IconButton";
-import { RxCaretDown } from "react-icons/rx";
 import AudioPlayer, { AudioPlayerStatus } from "../logic/AudioPlayer";
-import { TrackMeta } from "pipebomb.js/dist/music/Track";
-import { Text } from "@nextui-org/react";
-import { convertArrayToString } from "../logic/Utils";
-import { formatTime } from "../logic/Utils";
-import Waveform from "./Waveform";
-import PipeBombConnection from "../logic/PipeBombConnection";
+import { lerp } from "../logic/Utils";
+import PlaylistCoverSlide from "./PlaylistCoverSlide";
 
 let toggleFunction: (value: boolean) => void;
 
@@ -20,9 +14,9 @@ export default function PlayerCover() {
     const audioPlayer = AudioPlayer.getInstance();
     const [isOpen, setIsOpen] = useState(false);
     const [status, setStatus] = useState<AudioPlayerStatus>(audioPlayer.getStatus());
-    const [trackMeta, setTrackMeta] = useState<TrackMeta | null>(null);
-    const [tempPercentChange, setTempPercentChange] = useState<number | null>(null);
-    const waveform = useRef(null);
+    
+    
+    const container = useRef<any>(null);
 
     toggleFunction = setIsOpen;
 
@@ -34,27 +28,9 @@ export default function PlayerCover() {
         }
     }, []);
 
-    useEffect(() => {
-        if (!status.track) return;
-
-        status.track.getMetadata().then(setTrackMeta);  
-    }, [status.track]);
-
     if (isOpen && (!status || !status.track)) {
         setIsOpen(false);
     }
-
-    const backgroundImage = (() => {
-        if (!trackMeta) {
-            return; // load
-        }
-        if (!trackMeta.image) {
-            return; // no image
-        }
-        return `url(${trackMeta.image})`;
-    })();
-
-    const percentage = status.time / status.duration * 100;
 
     function togglePlay() {
         if (!status) return;
@@ -65,73 +41,89 @@ export default function PlayerCover() {
         }
     }
 
-    function waveformMouseDown(e: React.TouchEvent) {
-        if (!waveform.current) return;
-        const waveformElement: HTMLElement = waveform.current;
-        
-
+    function regularMouseDown(e: React.TouchEvent) {
         const startX = e.touches[0].clientX;
-        let newPercentage = percentage;
-
-        setTempPercentChange(percentage);
+        const startY = e.touches[0].clientY;
 
         let active = true;
+        let direction: "vertical" | "horizontal" | null = null;
+
+        let xOffset = 0;
 
         function move(e: TouchEvent) {
             if (!active) return;
-            const difference = startX - e.touches[0].clientX;
-            const percentChange = difference / waveformElement.clientWidth * 100;
-            newPercentage = Math.min(Math.max(percentage + percentChange, 0), 100);
-            setTempPercentChange(newPercentage);
+            
+            const deltaX = e.touches[0].clientX - startX;
+            const deltaY = e.touches[0].clientY - startY;
+
+            if (!direction) {
+                if (Math.abs(deltaX) > Math.abs(deltaY)) {
+                    direction = "horizontal";
+                } else {
+                    direction = "vertical";
+                }
+            }
+
+            const containerElement: HTMLElement = container.current;
+
+            if (direction == "horizontal") {
+                xOffset = deltaX;
+                containerElement.style.left = `calc(-100% + ${deltaX}px)`;
+            } else {
+
+            }
         }
 
         function mouseUp() {
             active = false;
             document.removeEventListener("touchend", mouseUp);
             document.removeEventListener("touchmove", move);
-            AudioPlayer.getInstance().setTime(newPercentage);
-            setTimeout(() => {
-                setTempPercentChange(null);
-            }, 50);
+
+            const containerElement: HTMLElement = container.current;
+            if (Math.abs(xOffset) > containerElement.clientWidth / 12) {
+                if (xOffset < 0) {
+                    lerp(xOffset, -containerElement.clientWidth / 3, 50, value => {
+                        containerElement.style.left = `calc(-100% + ${value}px)`;
+                    });
+                    setTimeout(() => {
+                        AudioPlayer.getInstance().nextTrack();
+                        setTimeout(() => {
+                            containerElement.style.left = "-100%";
+                        }, 0);
+                    }, 50);
+                } else {
+                    lerp(xOffset, containerElement.clientWidth / 3, 50, value => {
+                        containerElement.style.left = `calc(-100% + ${value}px)`;
+                    });
+                    setTimeout(() => {
+                        AudioPlayer.getInstance().previousTrack();
+                        setTimeout(() => {
+                            containerElement.style.left = "-100%";
+                        }, 0);
+                    }, 50);
+                }
+            } else {
+                lerp(xOffset, 0, 50, value => {
+                    containerElement.style.left = `calc(-100% + ${value}px)`;
+                });
+            }
         }
 
         document.addEventListener("touchmove", move);
         document.addEventListener("touchend", mouseUp);
     }
 
-    const usingPercentage = tempPercentChange === null ? percentage : tempPercentChange;
-
     return (
-        <div className={styles.container + (isOpen ? "" : ` ${styles.closed}`) + (status.paused ? "" : ` ${styles.playing}`)} onClick={togglePlay}>
-            <div className={styles.background} style={{
-                backgroundImage,
-                left: `${usingPercentage}%`,
-                transform: `translate(${-usingPercentage}%, -50%)`
-            }}></div>
-            <div className={styles.closeButton}>
-                <IconButton onPress={() => setIsOpen(false)} background small>
-                    <RxCaretDown />
-                </IconButton>
-            </div>
-            <div className={styles.topInfo}>
-                <span>
-                    <Text h2 className={styles.title}>
-                        {trackMeta?.title}
-                    </Text>
-                </span>
-                <span>
-                    <Text h3 className={styles.artist}>
-                        { convertArrayToString(trackMeta?.artists || []) }
-                    </Text>
-                </span>
-            </div>
-            <div className={styles.waveformMouseHandler} onTouchStart={waveformMouseDown}>
-                <div ref={waveform} className={styles.waveformContainer} style={{transform: `translateX(${-usingPercentage}%)`}}>
-                    <Waveform url={`${PipeBombConnection.getInstance().getUrl()}/v1/audio/${status.track?.trackID}`} active={!status.paused} percent={usingPercentage} />
-                </div>
-            </div>
-            <span className={styles.time}>{ formatTime(Math.max(0, status.duration * usingPercentage / 100)) }</span>
-            <span className={styles.duration}>{ formatTime(Math.max(status.duration, 0)) }</span>
+        <div ref={container} className={styles.container + (isOpen ? "" : ` ${styles.closed}`) + (status.paused ? "" : ` ${styles.playing}`)} onClick={togglePlay} onTouchStart={regularMouseDown}>
+            {status.track && (
+                <PlaylistCoverSlide track={status.track} />
+            )}
+            {status.track && (
+                <PlaylistCoverSlide track={status.track} status={status} />
+            )}
+            {status.track && (
+                <PlaylistCoverSlide track={status.queue[0] || status.track} />
+            )}
         </div>
     )
 }
---
+++ src/components/PlaylistCoverSlide.tsx
@@ -0,0 +1,108 @@
+import Track, { TrackMeta } from "pipebomb.js/dist/music/Track";
+import AudioPlayer, { AudioPlayerStatus } from "../logic/AudioPlayer";
+import styles from "../styles/PlayerCoverSlide.module.scss";
+import { useEffect, useRef, useState } from "react";
+import IconButton from "./IconButton";
+import { RxCaretDown } from "react-icons/rx";
+import { Text } from "@nextui-org/react";
+import { convertArrayToString, formatTime } from "../logic/Utils";
+import Waveform from "./Waveform";
+import PipeBombConnection from "../logic/PipeBombConnection";
+import { setPlayerOpen } from "./PlayerCover";
+
+export interface PlaylistCoverSlideProps {
+    track: Track,
+    status?: AudioPlayerStatus
+}
+
+export default function PlaylistCoverSlide({ track, status }: PlaylistCoverSlideProps) {
+    const [trackMeta, setTrackMeta] = useState<TrackMeta | null>(null);
+    const waveform = useRef(null);
+    const [tempPercentChange, setTempPercentChange] = useState<number | null>(null);
+
+    useEffect(() => {
+        track.getMetadata().then(setTrackMeta);  
+    }, [track]);
+
+    const backgroundImage = (() => {
+        if (!trackMeta) {
+            return; // load
+        }
+        if (!trackMeta.image) {
+            return; // no image
+        }
+        return `url(${trackMeta.image})`;
+    })();
+
+    const percentage = status ? (tempPercentChange === null ? (status.time / status.duration * 100) : tempPercentChange) : 0;
+
+    function waveformMouseDown(e: React.TouchEvent) {
+        if (!waveform.current) return;
+        const waveformElement: HTMLElement = waveform.current;
+        
+
+        const startX = e.touches[0].clientX;
+        let newPercentage = percentage;
+
+        setTempPercentChange(percentage);
+
+        let active = true;
+
+        function move(e: TouchEvent) {
+            if (!active) return;
+            e.stopImmediatePropagation();
+            const difference = startX - e.touches[0].clientX;
+            const percentChange = difference / waveformElement.clientWidth * 100;
+            newPercentage = Math.min(Math.max(percentage + percentChange, 0), 100);
+            setTempPercentChange(newPercentage);
+        }
+
+        function mouseUp() {
+            active = false;
+            document.removeEventListener("touchend", mouseUp);
+            document.removeEventListener("touchmove", move);
+            AudioPlayer.getInstance().setTime(newPercentage);
+            setTimeout(() => {
+                setTempPercentChange(null);
+            }, 50);
+        }
+
+        document.addEventListener("touchmove", move);
+        document.addEventListener("touchend", mouseUp);
+    }
+
+
+    return (
+        <div className={styles.slide}>
+            <div className={styles.background} style={{
+                backgroundImage,
+                left: `${percentage}%`,
+                transform: `translate(${-percentage}%, -50%)`
+            }}></div>
+            <div className={styles.closeButton}>
+                <IconButton onPress={() => setPlayerOpen(false)} background small>
+                    <RxCaretDown />
+                </IconButton>
+            </div>
+            <div className={styles.topInfo}>
+                <span>
+                    <Text h2 className={styles.title}>
+                        {trackMeta?.title}
+                    </Text>
+                </span>
+                <span>
+                    <Text h3 className={styles.artist}>
+                        { convertArrayToString(trackMeta?.artists || []) }
+                    </Text>
+                </span>
+            </div>
+            <div className={styles.waveformMouseHandler} onTouchStart={waveformMouseDown}>
+                <div ref={status ? waveform : null} className={styles.waveformContainer} style={{transform: `translateX(${-percentage}%)`}}>
+                    <Waveform url={`${PipeBombConnection.getInstance().getUrl()}/v1/audio/${status?.track?.trackID}`} active={status ? !status.paused : false} percent={percentage} />
+                </div>
+            </div>
+            <span className={styles.time}>{ formatTime(status ? Math.max(0, status.duration * percentage / 100) : 0) }</span>
+            <span className={styles.duration}>{ formatTime(status ? Math.max(status.duration, 0) : 0) }</span>
+        </div>
+    )
+}
