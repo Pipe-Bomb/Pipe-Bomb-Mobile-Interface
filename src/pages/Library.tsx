c6338e28f43f29aacd1adc3f736c2ac563904169
eyezah (via Perfect Girlfriend) <github@eyezah.com>
2023-04-20 15:22:29 +1200

    add search box to /library

--- src/pages/Library.tsx
+++ src/pages/Library.tsx
@@ -1,12 +1,17 @@
-import { useEffect, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import PlaylistIndex from "../logic/PlaylistIndex"
 import Loader from "../components/Loader";
 import ListPlaylist from "../components/ListPlaylist";
-import { Text } from "@nextui-org/react";
+import { Input, Text } from "@nextui-org/react";
 import styles from "../styles/Library.module.scss";
+
+let searchValue = "";
 
 export default function Library() {
     const playlistIndex = PlaylistIndex.getInstance();
+    const input = useRef<HTMLInputElement>(null);
+    const [search, setSearch] = useState(searchValue);
+    searchValue = search;
 
     const [playlists, setPlaylists] = useState(playlistIndex.getPlaylists());
 
@@ -25,14 +30,36 @@ export default function Library() {
             )
         }
 
-        return playlists.map((playlist, index) => (
-            <ListPlaylist playlist={playlist} key={index} />
-        ));
+        const elements: JSX.Element[] = [];
+
+        const lowerSearch = search.toLowerCase();
+        playlists.forEach(playlist => {
+            if (!search || playlist.getName().toLowerCase().includes(lowerSearch)) {
+                elements.push(
+                    <ListPlaylist playlist={playlist} key={elements.length} />
+                );
+            }
+        });
+
+        return elements;
     }
 
     return (
         <>
             <Text h1 className={styles.title}>Library</Text>
+            <div className={styles.search}>
+                <Input
+                    ref={input}
+                    size="xl"
+                    placeholder="Search playlists"
+                    clearable
+                    bordered
+                    fullWidth
+                    onChange={e => setSearch(e.target.value)}
+                    className={styles.bar}
+                    initialValue={search}
+                />
+            </div>
             { generatePlaylistHTML() }
         </>
     )
