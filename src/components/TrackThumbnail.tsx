5e8e78b5f39ea8261ce210f20f143999b3098327
eyezah <50851883+eyezahhhh@users.noreply.github.com>
2023-04-22 14:15:35 +1200

    allow holding on track thumbnails

--- src/components/TrackThumbnail.tsx
+++ src/components/TrackThumbnail.tsx
@@ -1,7 +1,12 @@
 import Track, { TrackMeta } from "pipebomb.js/dist/music/Track";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import Thumbnail from "./Thumbnail";
 import { convertArrayToString } from "../logic/Utils";
+import { ContextMenuEntry, openContextMenu } from "./ContextMenu";
+import { MdOutlineFileDownload, MdOutlinePlaylistAdd, MdPlaylistPlay, MdQueueMusic } from "react-icons/md";
+import AudioPlayer from "../logic/AudioPlayer";
+import LazyImage from "./LazyImage";
+import { openAddToPlaylist } from "./AddToPlaylist";
 
 export interface TrackThumbnailProps {
     track: Track
@@ -15,13 +20,55 @@ export default function TrackThumbnail({ track, small, onClick }: TrackThumbnail
     useEffect(() => {
         track.getMetadata().then(setTrackMeta);
     }, [track]);
+    
+    function hold() {
+        const options: ContextMenuEntry[] = [
+            {
+                icon: <MdPlaylistPlay />,
+                name: "Play next",
+                onPress: () => {
+                    AudioPlayer.getInstance().addToQueue([track], 0);
+                    return false;
+                }
+            },
+            {
+                icon: <MdQueueMusic />,
+                name: "Add to queue",
+                onPress: () => {
+                    AudioPlayer.getInstance().addToQueue([track]);
+                    return false;
+                }
+            },
+            {
+                icon: <MdOutlinePlaylistAdd />,
+                name: "Add to playlist",
+                onPress: () => {
+                    setTimeout(() => {
+                        openAddToPlaylist(track);
+                    }, 100);
+                    return false;
+                }
+            },
+            {
+                icon: <MdOutlineFileDownload />,
+                name: "Download",
+                disabled: true,
+                onPress: () => false
+            }
+        ];
+
+        openContextMenu({
+            title: trackMeta?.title || track.trackID,
+            subtitle: convertArrayToString(trackMeta?.artists || []),
+            image: trackMeta?.image ? <LazyImage src={trackMeta?.image} /> : null,
+            options
+        });
+    }
 
     if (!trackMeta) {
-        return (
-            <Thumbnail small={small} image="https://i1.sndcdn.com/artworks-000614969926-uqpcnd-large.jpg" title={"unset"} subtitle="Track" onClick={onClick} />
-        )
+        return null;
     }
     return (
-        <Thumbnail small={small} image={trackMeta.image || ""} title={trackMeta.title} subtitle={convertArrayToString(trackMeta.artists)} onClick={onClick} />
+        <Thumbnail small={small} image={trackMeta.image || ""} title={trackMeta.title} subtitle={convertArrayToString(trackMeta.artists)} onClick={onClick} onHold={hold} />
     )
 }
